105cad739ed880a480ae0b20523e96300ef80147
sobia <sobia.javed@arbisoft.com>
2019-12-24 07:25:06 +0500

    Add files via upload

--- test_register.py
+++ test_register.py
@@ -1,6 +1,9 @@
 import unittest
 from selenium import webdriver
 from pages.register import RegisterPage
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class EdxRegister(unittest.TestCase):
@@ -14,6 +17,10 @@ class EdxRegister(unittest.TestCase):
         self.assertTrue(self.register.is_browser_on_the_page())
         self.register.fill_form()
         self.register.submit_form()
+        WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'a.btn.btn-primary')))
+        browser_current_url = self.driver.current_url
+        self.assertEqual(browser_current_url, 'https://courses.stage.edx.org/dashboard')
 
     def tearDown(self):
         self.driver.close()
